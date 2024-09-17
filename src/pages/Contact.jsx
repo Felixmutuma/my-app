c9868ce8de0483ec15b9ac75fb5df4d35646b0ea
Felix John <felicmutuma8@gmail.com>
2024-09-17 19:48:57 +0300

    commits all changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -34,29 +34,40 @@ const Contact = () => {
     };
 
     return (
-<div className="contact-form-container">
-    <h1>Contact Me</h1>
-    <form onSubmit={handleSubmit}>
-        <label className="form-label">
-            Name:
-            <input type="text" name="name" value={formData.name} onChange={handleChange} required className="form-input" />
-        </label>
-        <label className="form-label">
-            Email:
-            <input type="email" name="email" value={formData.email} onChange={handleChange} required className="form-input" />
-        </label>
-        <label className="form-label">
-            Subject:
-            <input type="text" name="subject" value={formData.subject} onChange={handleChange} required className="form-input" />
-        </label>
-        <label className="form-label">
-            Message:
-            <textarea name="message" value={formData.message} onChange={handleChange} required className="form-textarea"></textarea>
-        </label>
-        <button type="submit" className="form-button">Send</button>
-    </form>
-</div>
-
+        <>
+                <nav>
+            <h2 className='logo'>FELIX <span>JOHN</span></h2>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/services">Services</a></li>
+            </ul>
+            <button className='btn'>Contact me</button>
+        </nav>
+        <div className="contact-form-container">
+            <h1>Contact Me</h1>
+            <form onSubmit={handleSubmit}>
+                <label className="form-label">
+                    Name:
+                    <input type="text" name="name" value={formData.name} onChange={handleChange} required className="form-input" />
+                </label>
+                <label className="form-label">
+                    Email:
+                    <input type="email" name="email" value={formData.email} onChange={handleChange} required className="form-input" />
+                </label>
+                <label className="form-label">
+                    Subject:
+                    <input type="text" name="subject" value={formData.subject} onChange={handleChange} required className="form-input" />
+                </label>
+                <label className="form-label">
+                    Message:
+                    <textarea name="message" value={formData.message} onChange={handleChange} required className="form-textarea"></textarea>
+                </label>
+                <button type="submit" className="form-button">Send</button>
+            </form>
+        </div>
+        </>
     );
 };
 
