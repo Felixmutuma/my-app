f1b38f3d1dd0ce6b4c183e5d285fafdc66decd07
Felix John <felicmutuma8@gmail.com>
2024-10-21 05:31:22 -0800

    commits updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -38,6 +38,7 @@ function Main() {
 
             </section>
         </main>
+    
     </>
      );
 }
--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -12,17 +12,20 @@ function ProjectsList() {
                 <h4>SpaceHub</h4>
                 <p>Space Hub is an online platform where you can find and book spaces online.</p>
             </div>
+            
+            <div className="project">
+                <a href="https://algo-trader.netlify.app/" target="_blank" rel="noopener noreferrer"> <img src="/assets/images/AlgoTrader.png" alt="Food Court Image" /></a>
+               
+                <h4>Forex AlgoTrader</h4>          
+                <p>Forex Algo-Trading is a site designed to help spread forex - foreign exchange - knowledge, skills, and ideas among the newbies, intermediate, professional, and advanced forex traders accross the globe.</p>
+            </div>
+
             <div className="project">
                 <img src="/assets/images/Food-court.jpeg" alt="Food Court Image" />
                 <h4>Food Court</h4>          
                 <p>Food Court is a web application for booking meals and in a restaraunt.</p>
             </div>
             
-            <div className="project">
-                <img src="/assets/images/AlgoTrader.png" alt="Food Court Image" />
-                <h4>Forex AlgoTrader</h4>          
-                <p>Forex Algo-Trading is a site designed to help spread forex - foreign exchange - knowledge, skills, and ideas among the newbies, intermediate, professional, and advanced forex traders accross the globe.</p>
-            </div>
             <div className="project">
                 <img src="/assets/images/Bulls-club.jpeg" alt="Bulls Club image" />
                 <h4>Bulls Club</h4>          
